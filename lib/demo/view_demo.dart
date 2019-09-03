736e27bd5939517aebec403b453185610e062686
wayfreem <wayfreem@163.com>
2019-09-03 22:50:53 +0800

    增加 GridView 显示的方式

--- lib/demo/view_demo.dart
+++ lib/demo/view_demo.dart
@@ -2,43 +2,122 @@ import 'package:flutter/material.dart';
 import '../model/post.dart';
 
 class ViewDemo extends StatelessWidget{
-  
+
   @override
   Widget build(BuildContext context) {
+    return GridViewBuilderDemo();
+  }
+}
 
-    Widget _pageItemBuilder(BuildContext context, int index){
-      return Stack(
-        children: <Widget>[
-          SizedBox.expand(
-            child: Image.network(posts[index].imageUrl, fit: BoxFit.cover),
-          ),
-          Positioned(
-              bottom: 8.0,
-              left: 8.0,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(posts[index].title,
-                      style: TextStyle(fontWeight: FontWeight.bold)),
-                  Text(posts[index].author),
-                ],
-              ))
-        ],
+class GridViewBuilderDemo extends StatelessWidget{
+
+  Widget _gridItemBuilder(BuildContext context, int index){
+    return Container(
+      child: Image.network(
+        posts[index].imageUrl, fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+        itemBuilder: _gridItemBuilder,
+        padding: EdgeInsets.all(8.0),
+        itemCount: posts.length,
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          // crossAxisCount: 3,
+          maxCrossAxisExtent: 150.0,
+          crossAxisSpacing: 8.0,
+          mainAxisSpacing: 8.0,
+        ),
+    );
+  }
+}
+
+class GridViewExtentDemo extends StatelessWidget{
+
+  List<Widget> _builderTiles(int length){
+    return List.generate(length, (int index){
+      return Container(
+        color: Colors.grey[300],
+        alignment: Alignment(0.0, 0.0),
+        child: Text('Item $index',
+            style: TextStyle(fontSize: 18.0, color: Colors.grey)),
       );
-    }
+    });
+  }
 
+  @override
+  Widget build(BuildContext context) {
+    return GridView.extent(
+      maxCrossAxisExtent: 100,    //  GridView.extent 特有的属性值
+      crossAxisSpacing: 16.0,
+      mainAxisSpacing: 16.0,
+      scrollDirection: Axis.vertical,
+      children: _builderTiles(100),
+    );
+  }
+}
+
+class GridViewCountDemo extends StatelessWidget{
+
+  List<Widget> _builderTiles(int length){
+    return List.generate(length, (int index){
+      return Container(
+        color: Colors.grey[300],
+        alignment: Alignment(0.0, 0.0),
+        child: Text('Item $index',
+            style: TextStyle(fontSize: 18.0, color: Colors.grey)),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      crossAxisCount: 3,    //  GridView.count 特有的属性值
+      crossAxisSpacing: 16.0,
+      mainAxisSpacing: 16.0,
+      scrollDirection: Axis.vertical,
+      children: _builderTiles(100),
+    );
+  }
+}
+
+class PageViewDemoBuilderDemo extends StatelessWidget{
+
+  Widget _pageItemBuilder(BuildContext context, int index){
+    return Stack(
+      children: <Widget>[
+        SizedBox.expand(
+          child: Image.network(posts[index].imageUrl, fit: BoxFit.cover),
+        ),
+        Positioned(
+            bottom: 8.0,
+            left: 8.0,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(posts[index].title,
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+                Text(posts[index].author),
+              ],
+            ))
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return PageView.builder(
       itemCount: posts.length,
       itemBuilder: _pageItemBuilder,
     );
   }
-
 }
 
-
 class PageViewDemo extends StatelessWidget{
-
-
   @override
   Widget build(BuildContext context) {
 
